64837046272dcc75800df220f8fd45b885fff8e2
marcusvlc <marcus.costa@ccc.ufcg.edu.br>
2024-04-28 16:19:45 -0300

    fix: Remove unsed code

--- web-form/src/utils/Validator.js
+++ web-form/src/utils/Validator.js
@@ -1,40 +1,40 @@
 class Validator {
-    isValidEmail(email) {
-        const emailRegex = /\S+@\S+\.\S+/;
-        return emailRegex.test(email)
-    }
+  isValidEmail(email) {
+    const emailRegex = /\S+@\S+\.\S+/;
+    return emailRegex.test(email);
+  }
 
-    isValidCPF(cpf) {
-        const cpfRegex = /^\d{3}\.?\d{3}\.?\d{3}\-?\d{2}$/
-        return cpfRegex.test(cpf)
-    }
+  isValidCPF(cpf) {
+    const cpfRegex = /^\d{3}\.?\d{3}\.?\d{3}\-?\d{2}$/;
+    return cpfRegex.test(cpf);
+  }
 
-    isValidCNPJ(cnpj) {
-        const cnpjRegex = /^\d{2}\.?\d{3}\.?\d{3}\/?\d{4}\-?\d{2}$/
-        console.log(cnpj, cnpjRegex.test(cnpj));
-        return cnpjRegex.test(cnpj)
-    }
+  isValidCNPJ(cnpj) {
+    const cnpjRegex = /^\d{2}\.?\d{3}\.?\d{3}\/?\d{4}\-?\d{2}$/;
+    return cnpjRegex.test(cnpj);
+  }
 
-    isValidPhoneNumber(phone) {
-        const phoneRegex = /\(?([0-9]{2,3}|0{1}((x|[0-9]){2,3}[0-9]{2}))\)?\s*[0-9]{4,5}[- ]*[0-9]{4}/
-        return phoneRegex.test(phone)
-    }
+  isValidPhoneNumber(phone) {
+    const phoneRegex =
+      /\(?([0-9]{2,3}|0{1}((x|[0-9]){2,3}[0-9]{2}))\)?\s*[0-9]{4,5}[- ]*[0-9]{4}/;
+    return phoneRegex.test(phone);
+  }
 
-    isValidDate(dateStr) {
-        if(!dateStr) return false
+  isValidDate(dateStr) {
+    if (!dateStr) return false;
 
-        const dtToday = new Date()
+    const dtToday = new Date();
 
-        return new Date(dateStr) < dtToday;
-    }
+    return new Date(dateStr) < dtToday;
+  }
 
-    isValidPassword(password) {
-        return !!password
-    }
+  isValidPassword(password) {
+    return !!password;
+  }
 
-    getFieldsWithDiffValue(obj, objToCompare, keysList) {
-        return keysList.filter(key => obj[key] !== objToCompare[key])
-    }
+  getFieldsWithDiffValue(obj, objToCompare, keysList) {
+    return keysList.filter((key) => obj[key] !== objToCompare[key]);
+  }
 }
 
-export default new Validator()
+export default new Validator();
